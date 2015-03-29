483e47f668b7092152036d76426514adc8e6c7c7
robertooliveiras <robertooliveira2604@gmail.com>
2015-03-29 19:43:47 -0300

    M9Ex1 complemento - Filter incompleto

--- module/Market/src/Market/Form/PostFilter.php
+++ module/Market/src/Market/Form/PostFilter.php
@@ -4,6 +4,7 @@ namespace Market\Form;
 use Zend\InputFilter\InputFilter;
 use Zend\InputFilter\Input;
 use Zend\Validator\Regex;
+use Zend\Validator\Date;
 
 class PostFilter extends InputFilter
 {
@@ -16,28 +17,53 @@ class PostFilter extends InputFilter
     
     public function buildFilter() 
     {
+    	//category char 16 not null,
         $category = new Input('category');
-        $category->getFilterChain()
-                 ->attachByName('StringTrim')
-                 ->attachByName('StripTags')
-                 ->attachByName('StringToLower');
-        $category->getValidatorChain()
-                 ->attachByName('inArray',array('haystack'=>$this->categories));
+        	$category->setAllowEmpty(TRUE);
+	        $category->getFilterChain()
+	                 ->attachByName('StringTrim')
+	                 ->attachByName('StripTags')
+	                 ->attachByName('StringToLower');
+	        $category->getValidatorChain()
+	                 ->attachByName('inArray',array('haystack'=>$this->categories));
         
+        //title varchar 128 not null
         $title = new Input('title');
-        $title->getFilterChain()
-              ->attachByName('StringTrim')
-              ->attachByName('StripTags');
+        	$title->setAllowEmpty(TRUE);
+	        $title->getFilterChain()
+	              ->attachByName('StringTrim')
+	              ->attachByName('StripTags');
+	        
+	        $titleRegex = new Regex(array('pattern'=>'/^[a-zA-Z0-9 ]*$/'));
+	        $titleRegex->setMessage('Title should contain only numbers, letters or spaces.');
+	        
+	        $title->getValidatorChain()
+	              ->attach($titleRegex)
+	              ->attachByName('StringLength',array('min'=>1,'max'=>128));
         
-        $titleRegex = new Regex(array('pattern'=>'/^[a-zA-Z0-9 ]*$/'));
-        $titleRegex->setMessage('Title should contain only numbers, letters or spaces.');
-        
-        $title->getValidatorChain()
-              ->attach($titleRegex)
-              ->attachByName('StringLength',array('min'=>1,'max'=>128));
+        //date_created timestamp not null default current_timestamp
+	    $dateCreated = new Input('dateCreated');
+	    $dateCreated->getFilterChain()
+	    			->attachByName('StringTrim')
+	    			->attachByName('StripTags');
+	    $dateCreated->getValidatorChain()
+	    			->attachByName('StringLength',array('min'=>10,'max'=>10));
+	    
+		// 	    date_expires timestamp not null default null
+		// 	    descripton varchar 4096 default null
+		// 	    photo_filename varchar 1024 default null
+		// 	    contact_name varchar 255 default null
+		// 	    contact_email varchar 255 default null
+		// 	    contact_phone varchar 32 default null
+		// 	    city varchar 128 default null
+		// 	    country char 2 not null
+		// 	    price decimal 12,2 not null
+		// 	    delete_code char 16 character set utf8 collate utf8_bin default null
+	    
         
         $this->add($category)
-             ->add($title);
+             ->add($title)
+        	 ->add($dateCreated);
     }
 }
 
--- module/Market/src/Market/Form/PostForm.php
+++ module/Market/src/Market/Form/PostForm.php
@@ -4,7 +4,10 @@ namespace Market\Form;
 use Zend\Form\Form;
 use Zend\Form\Element\Select;
 use Zend\Form\Element\Text;
+use Zend\Form\Element\Textarea;
 use Zend\Form\Element\Submit;
+use Zend\Form\Element\Date;
+use Zend\Form\Element\Email	;
 
 class PostForm extends Form 
 {
@@ -33,15 +36,71 @@ class PostForm extends Form
                         array_combine($this->categories, $this->categories)
                      );
         
+        //Zend\Form\Element\Text
         $title = new Text('title');
         $title->setLabel("Title")
               ->setAttributes(array('size'=>55, 'maxLength'=>128));
         
-        $submit = new Submit('submit');
+        $dateCreated = new Date('dateCreated');
+        $dateCreated->setLabel("Date Created")
+              ->setAttributes(array('size'=>20, 'maxLength'=>10));
+        
+        $dateExpires = new Date('dateExpires');
+        $dateExpires->setLabel("Date Expires")
+              ->setAttributes(array('size'=>20, 'maxLength'=>10));
+        
+        $descripton = new Textarea('descripton');
+        $descripton->setLabel("Descripton")
+              ->setAttributes(array('rows'=>4, 'cols'=>80, 'maxLength'=>4096));
+        
+        $photoFilename = new Textarea('photoFilename');
+        $photoFilename->setLabel("Photo Filename")
+              ->setAttributes(array('rows'=>4, 'cols'=>80, 'maxLength'=>1024));
+        
+        $contactName = new Text('contactName');
+        $contactName->setLabel("Contact Name")
+              ->setAttributes(array('size'=>40, 'maxLength'=>255));
+        
+        $contactEmail = new Email('contactEmail');
+        $contactEmail->setLabel("Contact Email")
+              ->setAttributes(array('size'=>40, 'maxLength'=>255));
+        
+        $contactPhone = new Text('contactPhone');
+        $contactPhone->setLabel("Contact Phone")
+              ->setAttributes(array('size'=>20, 'maxLength'=>32));
+        
+        $city = new Text('city');
+        $city->setLabel("City")
+              ->setAttributes(array('size'=>40, 'maxLength'=>128));
+        
+        $country = new Text('country');
+        $country->setLabel("Country")
+              ->setAttributes(array('size'=>2, 'maxLength'=>2));
+        
+        $price = new Text('price');
+        $price->setLabel("Price")
+              ->setAttributes(array('size'=>16, 'maxLength'=>16));
+        
+//         $deleteCode = new Text('deleteCode');
+//         $deleteCode->setLabel("Delete Code")
+//               ->setAttributes(array('size'=>30, 'maxLength'=>16));
+        
+        $submit = new Submit('submit');                
         $submit->setAttribute('value', 'Post');
         
         $this->add($category)
              ->add($title)
+             ->add($dateCreated)
+             ->add($dateExpires)
+             ->add($descripton)
+             ->add($photoFilename)
+             ->add($contactName)
+             ->add($contactPhone)
+             ->add($contactEmail)
+             ->add($city)
+             ->add($country)
+             ->add($price)
+//              ->add($deleteCode)
              ->add($submit);
     }
 }
